9969d7d60aca967a193cb7055b63053b5a3b4d80
Ágoston Székely <agoston.szekely@egroup.hu>
2017-01-05 17:18:04 +0100

    új activity nfc-re

---
+++ app/src/main/java/com/gusztafszon/eszigreader/activities/NfcActivity.java
@@ -0,0 +1,8 @@
+package com.gusztafszon.eszigreader.activities;
+
+/**
+ * Created by agoston.szekely on 2017.01.05..
+ */
+
+public class NfcActivity {
+}
---
+++ app/src/main/java/com/gusztafszon/eszigreader/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.gusztafszon.eszigreader.constants;
+
+/**
+ * Created by agoston.szekely on 2017.01.05..
+ */
+
+public interface Constants {
+}
